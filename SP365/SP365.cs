c690e1d5b7dbc81d77742ffd7fb1af16c7dbc724
Cross <crossleyjuan@gmail.com>
2015-02-09 14:51:15 +0000

    Initial Commit

---
+++ SP365/SP365.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Client;
+using System.Security;
+
+namespace BizagiCL
+{
+    public class SP365
+    {
+        private static Folder GetFolder(ClientContext clientContext, string parentFolder, string subFolder)
+        {
+            Web web = clientContext.Web;
+
+            clientContext.Load(web);
+
+            // Loads the folder
+            List docs = web.Lists.GetByTitle(parentFolder);
+            Folder spFolder;
+            if (!string.IsNullOrEmpty(subFolder))
+            {
+                clientContext.Load(docs.RootFolder);
+                clientContext.ExecuteQuery();
+                string url = docs.RootFolder.Name;
+                url += "/" + subFolder;
+
+                try
+                {
+                    spFolder = web.GetFolderByServerRelativeUrl(url);
+                    clientContext.Load(spFolder);
+                    clientContext.ExecuteQuery();
+                }
+                catch
+                {
+                    internalCreateFolder(clientContext, parentFolder, subFolder);
+                    spFolder = web.GetFolderByServerRelativeUrl(url);
+                    clientContext.Load(spFolder);
+                    clientContext.ExecuteQuery();
+                }
+
+            }
+            else
+            {
+                spFolder = docs.RootFolder;
+                clientContext.Load(spFolder);
+            }
+
+            return spFolder;
+        }
+
+        public static void PublishFile(string website, sbyte[] sdata, string fileName, string userName, string password, string parentFolder, string folder)
+        {
+            byte[] data = new byte[sdata.Length];
+            Buffer.BlockCopy(sdata, 0, data, 0, sdata.Length);
+
+            using (ClientContext clientContext = new ClientContext(website))
+            {
+                SecureString passWord = new SecureString();
+
+                foreach (char c in password.ToCharArray()) passWord.AppendChar(c);
+
+                clientContext.Credentials = new SharePointOnlineCredentials(userName, passWord);
+
+                Web web = clientContext.Web;
+
+                clientContext.Load(web);
+
+                FileCreationInformation newFile = new FileCreationInformation();
+                newFile.Content = data;
+                string fileNameref = fileName;
+                if (fileNameref.IndexOf("\\") > 0)
+                {
+                    fileNameref = fileNameref.Substring(fileNameref.LastIndexOf("\\") + 1);
+                }
+                //if (folder.IndexOf("/") > -1)
+                //{
+                //    fileNameref = folder.Substring(folder.IndexOf("/") + 1) + "/" + fileNameref;
+                //    folder = folder.Substring(0, folder.LastIndexOf("/"));
+                //}
+                newFile.Url = fileNameref;
+
+                Folder spFolder = GetFolder(clientContext, parentFolder, folder);
+                //List docs = web.Lists.GetByTitle(folder);
+                Microsoft.SharePoint.Client.File uploadFile = spFolder.Files.Add(newFile);
+                clientContext.Load(uploadFile);
+                clientContext.ExecuteQuery();
+            }
+        }
+
+        public static void DeleteFile(string website, string fileName, string userName, string password, string folder, string subFolder)
+        {
+            using (ClientContext clientContext = new ClientContext(website))
+            {
+                SecureString passWord = new SecureString();
+
+                foreach (char c in password.ToCharArray()) passWord.AppendChar(c);
+
+                clientContext.Credentials = new SharePointOnlineCredentials(userName, passWord);
+
+                Web web = clientContext.Web;
+
+                clientContext.Load(web);
+
+                string fileNameref = fileName;
+                if (fileNameref.IndexOf("\\") > 0)
+                {
+                    fileNameref = fileNameref.Substring(fileNameref.LastIndexOf("\\") + 1);
+                }
+                // Get list document
+                Folder sFolder = GetFolder(clientContext, folder, subFolder);
+
+                FileCollection files = sFolder.Files;
+                clientContext.Load(files);
+                clientContext.ExecuteQuery();
+
+                foreach (File file in files)
+                {
+                    if (file.Name == fileNameref)
+                    {
+                        file.DeleteObject();
+                        break;
+                    }
+                }
+                clientContext.ExecuteQuery();
+            }
+        }
+
+        private static void internalCreateFolder(ClientContext clientContext, string parentFolder, string folder)
+        {
+            Web web = clientContext.Web;
+
+            clientContext.Load(web);
+
+            List docs = web.Lists.GetByTitle(parentFolder);
+            //clientContext.Load(docs);
+            ListItemCreationInformation list = new ListItemCreationInformation();
+            Folder spFolder = docs.RootFolder.Folders.Add(folder);
+
+            docs.Update();
+
+            clientContext.ExecuteQuery();
+
+        }
+
+        public static void CreateFolder(string website, string userName, string password, string parentFolder, string newFolder)
+        {
+            using (ClientContext clientContext = new ClientContext(website))
+            {
+                SecureString passWord = new SecureString();
+
+                foreach (char c in password.ToCharArray()) passWord.AppendChar(c);
+
+                clientContext.Credentials = new SharePointOnlineCredentials(userName, passWord);
+
+                internalCreateFolder(clientContext, parentFolder, newFolder);
+            }
+        }
+    }
+}
---
+++ Sharepoint365/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Client;
+using System.Security;
+using System.Configuration;
+
+namespace Sharepoint365
+{
+    class Program
+    {
+        static string website = ConfigurationSettings.AppSettings["Sharepoint_URL"];
+        static string defaultUser = ConfigurationSettings.AppSettings["DefaultUser"];
+        static string defaultPassword = ConfigurationSettings.AppSettings["DefaultPassword"];
+
+        static string ReadValue(string text, string defaultValue)
+        {
+            Console.Write(string.Format("{0} ({1}): ", text, defaultValue));
+            string value = Console.ReadLine();
+            if (string.IsNullOrEmpty(value))
+            {
+                value = defaultValue;
+            }
+
+            return value;
+        }
+
+        static void testNewFile()
+        {
+            try
+            {
+
+
+                Console.Write(string.Format("Username ({0}):", defaultUser));
+                string user = Console.ReadLine();
+                if (string.IsNullOrEmpty(user))
+                {
+                    user = defaultUser;
+                }
+
+                Console.Write(string.Format("Password ({0}):", defaultPassword));
+                string password = Console.ReadLine();
+                if (string.IsNullOrEmpty(password))
+                {
+                    password = defaultPassword;
+                }
+
+                Console.Write("File to upload (c:\\temp\\I did it.jpg");
+                string file = Console.ReadLine();
+                if (string.IsNullOrEmpty(file))
+                {
+                    file = "c:\\temp\\I did it.jpg";
+                }
+
+                string folder = ReadValue("Folder", null);
+
+                byte[] content = System.IO.File.ReadAllBytes(file);
+
+                sbyte[] scontent = new sbyte[content.Length];
+
+                Buffer.BlockCopy(content, 0, scontent, 0, content.Length);
+
+                Console.WriteLine("Uploading file....");
+                BizagiCL.SP365.PublishFile(website, scontent, file, user, password, "Documents", folder);
+                Console.WriteLine("File uploaded");
+
+            }
+            catch (Exception e)
+            {
+                Console.Write("Error: " + e.Message);
+            }
+            Console.ReadLine();
+        }
+
+        static void TestDelete(string user, string password)
+        {
+            try
+            {
+                string file = ReadValue("File to delete", "c:\\temp\\I did it.jpg");
+
+                string folder = ReadValue("Folder", null);
+
+                Console.WriteLine("Deleting file....");
+                BizagiCL.SP365.DeleteFile(website, file, user, password, "Documents", folder);
+                Console.WriteLine("Deleted. Done");
+            }
+            catch (Exception e)
+            {
+                Console.Write("Error: " + e.Message);
+            }
+            Console.ReadLine();
+        }
+
+
+        static void TestFolder()
+        {
+            Console.Write(string.Format("Username ({0}):", defaultUser));
+            string user = Console.ReadLine();
+            if (string.IsNullOrEmpty(user))
+            {
+                user = defaultUser;
+            }
+
+            Console.Write(string.Format("Password ({0}):", defaultPassword));
+            string password = Console.ReadLine();
+            if (string.IsNullOrEmpty(password))
+            {
+                password = defaultPassword;
+            }
+
+            Console.Write("Folder to create (TestFolder)");
+            string file = Console.ReadLine();
+            if (string.IsNullOrEmpty(file))
+            {
+                file = "TestFolder";
+            }
+
+            Console.WriteLine("Deleting file....");
+            BizagiCL.SP365.CreateFolder(website, user, password, "Documents", file);
+            Console.WriteLine("Deleted. Done");
+        }
+
+        static void testEmptyFolder()
+        {
+            Console.Write("File to upload (c:\\temp\\I did it.jpg");
+            string file = Console.ReadLine();
+            if (string.IsNullOrEmpty(file))
+            {
+                file = "c:\\temp\\I did it.jpg";
+            }
+
+            byte[] content = System.IO.File.ReadAllBytes(file);
+
+            sbyte[] scontent = new sbyte[content.Length];
+
+            Buffer.BlockCopy(content, 0, scontent, 0, content.Length);
+
+            BizagiCL.SP365.PublishFile(website, scontent, file, defaultUser, defaultPassword, "Documents", null);
+        }
+
+        static void Main(string[] args)
+        {
+//            TestFolder();
+            //testNewFile();
+
+            string user = ReadValue("User", defaultUser);
+            string password = ReadValue("Password", defaultPassword);
+            TestDelete(user, password);
+            Console.ReadLine();
+        }
+    }
+}
